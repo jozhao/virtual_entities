4d61ccedaf5ea886a428822306dd1190da3c76e2
Joseph Zhao <joseph.zhao@xing.net.au>
2017-04-20 23:46:55 +1000

    The @return tag is followed by an optional data type indicator, and then a newline

--- src/VirtualEntityTypeInterface.php
+++ src/VirtualEntityTypeInterface.php
@@ -5,7 +5,7 @@ namespace Drupal\virtual_entities;
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
 
 /**
- * Interface VirtualEntityTypeInterface
+ * Interface VirtualEntityTypeInterface.
  *
  * @package Drupal\virtual_entities
  */
@@ -14,21 +14,24 @@ interface VirtualEntityTypeInterface extends ConfigEntityInterface {
   /**
    * Gets the description.
    *
-   * @return string The description of this entity type.
+   * @return string
+   *   The description of this entity type.
    */
   public function getDescription();
 
   /**
    * Gets the help information.
    *
-   * @return string The help information of this entity type.
+   * @return string
+   *  The help information of this entity type.
    */
   public function getHelp();
 
   /**
    * Gets the client endpoint.
    *
-   * @return string The client endpoint of this entity type.
+   * @return string
+   *  The client endpoint of this entity type.
    */
   public function getEndpoint();
 
