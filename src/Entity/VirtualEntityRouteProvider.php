789ed4a5174749f41372791c007d4bbb78ba6099
Joseph Zhao <joseph.zhao@xing.net.au>
2017-04-21 17:05:55 +1000

    Add entity type access controller

--- src/Entity/VirtualEntityRouteProvider.php
+++ src/Entity/VirtualEntityRouteProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\virtual_entities;
+namespace Drupal\virtual_entities\Entity;
 
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\Routing\EntityRouteProviderInterface;
