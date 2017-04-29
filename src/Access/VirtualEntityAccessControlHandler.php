fc09f9e67370c2994493921f1383ec6b0455f271
Joseph Zhao <joseph.zhao@xing.net.au>
2017-04-29 14:40:43 +1000

    Update entity access rules

--- src/Access/VirtualEntityAccessControlHandler.php
+++ src/Access/VirtualEntityAccessControlHandler.php
@@ -5,6 +5,7 @@ namespace Drupal\virtual_entities\Access;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
 
 /**
  * Class VirtualEntityAccessControlHandler.
@@ -16,13 +17,35 @@ class VirtualEntityAccessControlHandler extends EntityAccessControlHandler {
   /**
    * {@inheritdoc}
    */
+  public function access(EntityInterface $entity, $operation, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    $result = parent::access($entity, $operation, $account, TRUE);
+    if ($result->isForbidden()) {
+      return $return_as_object ? $result : $result->isAllowed();
+    }
+    $permission = $operation === 'update' ? "edit {$entity->bundle()} virtual entity" : "{$operation} {$entity->bundle()} virtual entity";
+    $result = AccessResult::allowedIfHasPermission($account, $permission);
+
+    return $return_as_object ? $result : $result->isAllowed();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     $result = parent::checkAccess($entity, $operation, $account);
     if ($result->isForbidden()) {
       return $result;
     }
 
-    return $result;
+    // No opinion.
+    return AccessResult::neutral();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
+    return AccessResult::allowedIfHasPermission($account, 'add contact entity');
   }
 
 }
